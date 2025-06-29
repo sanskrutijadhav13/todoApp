780b15f44f2e9f4af3e0e09560b2258268423dbf
sanskrutijadhav13 <sanskrutijadhav1315@gmail.com>
2025-06-29 10:39:48 +0530

    second update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
-export default function App(){
-  return(
-    <h1>Hello world</h1>
+ function App(){
+  return (
+   <center className = "todo-container">
+     <h1>TODO App</h1>
+       <div className="container text-center">
+          <div className="row">
+            <div className="col-6">
+              <input type="text" placeholder="Enter Todo Task" />
+            </div>
+            <div className="col-4">
+              <input type="date" />
+            </div>
+            <div className="col-2">
+              <button type="button" class="btn btn-success">Add</button>
+            </div>
+          </div>
+        </div>
+    </center>  
   )
+  
 }
+
+export default App
